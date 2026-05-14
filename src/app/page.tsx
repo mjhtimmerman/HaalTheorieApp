05baa7a1e79c56398a15601bcf6bbfbe478f193e
mjhtimmerman <232400785+mjhtimmerman@users.noreply.github.com>
2026-05-14 18:03:06 +0200

    Homepage direct to dashboard

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,5 @@
-const Homepage = () => {
-  return (
-    <div className=''>Homepage</div>
-  )
-}
+import { redirect } from "next/navigation";
 
-export default Homepage
+export default function HomePage() {
+  redirect("/dashboard/admin");
+}
